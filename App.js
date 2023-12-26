4e6d37409ed14c7a2defb076255245c1d5b65d3c
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2023-12-26 14:33:17 +0700

    first setup

---
+++ App.js
@@ -0,0 +1,22 @@
+import { React } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import LoginScreen from "./app/screens/LoginScreen";
+
+export default function App() {
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+function LoginScreen(props) {
+  return;
+  <View style={styles.container}>
+    <Text>{Hello}</Text>
+  </View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+});
+
+export default LoginScreen;
